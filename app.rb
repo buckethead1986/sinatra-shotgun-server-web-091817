79d89d4ddb178f9045ed054879035d7aec030909
buckethead1986 <rspeckpfs@gmail.com>
2017-10-08 15:35:27 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    "Welcome to your app!!!! Come out virginia you don't have to wait"
   end
 
 end
